41d8be47571325d487e7cb421b36fee39dcdced0
Yixie2020 <Yixie2020@users.noreply.github.com>
2026-09-08 16:05:10 +0800

    ci: hardcode AyuGram API credentials, simplify workflows
    - Set default TDESKTOP_API_ID=611335 and TDESKTOP_API_HASH in cmake
    - Remove API secret requirements from CI workflows
    - Workflows now work without any secrets configuration

--- Telegram/cmake/telegram_options.cmake
+++ Telegram/cmake/telegram_options.cmake
@@ -5,8 +5,8 @@
 # https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 option(TDESKTOP_API_TEST "Use test API credentials." OFF)
-set(TDESKTOP_API_ID "0" CACHE STRING "Provide 'api_id' for the Telegram API access.")
-set(TDESKTOP_API_HASH "" CACHE STRING "Provide 'api_hash' for the Telegram API access.")
+set(TDESKTOP_API_ID "611335" CACHE STRING "Provide 'api_id' for the Telegram API access.")
+set(TDESKTOP_API_HASH "d524b414d21f4d37f08684c1df41ac9c" CACHE STRING "Provide 'api_hash' for the Telegram API access.")
 
 if (TDESKTOP_API_TEST)
     set(TDESKTOP_API_ID 17349)
